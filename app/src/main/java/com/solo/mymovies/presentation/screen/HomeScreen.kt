328dae8d5a1571dbb5dd0bc5af4e94ec19e99e4b
Darko <darkoangelovski666@gmail.com>
2025-05-25 21:40:16 +0200

    implemented movie and series search endpoint and ui part

--- app/src/main/java/com/solo/mymovies/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/solo/mymovies/presentation/screen/HomeScreen.kt
@@ -10,11 +10,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,24 +28,42 @@ import coil.compose.AsyncImage
 import com.solo.mymovies.domain.model.PopularMovie
 import com.solo.mymovies.presentation.MovieViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: MovieViewModel = hiltViewModel(),
-    onNavigateToDetails: (movieId: Int) -> Unit
+    onNavigateToDetails: (movieId: Int) -> Unit,
+    onNavigateToSearch: () -> Unit
 ) {
     val movies = viewModel.popularMovies.collectAsLazyPagingItems()
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 16.dp)
-    ) {
-        items(movies.itemCount) { index ->
-            val movie = movies[index]
-            if (movie != null) {
-                MovieItem(movie = movie, onClick = {
-                    onNavigateToDetails(movie.id)
-                })
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Popular Movies") },
+                actions = {
+                    IconButton(onClick = { onNavigateToSearch() }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            items(movies.itemCount) { index ->
+                val movie = movies[index]
+                if (movie != null) {
+                    MovieItem(movie = movie, onClick = {
+                        onNavigateToDetails(movie.id)
+                    })
+                }
             }
         }
     }
